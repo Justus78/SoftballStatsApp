2b162ba577f6a1e84fc802750c9c55397153650c
Justus78 <stars787878@aol.com>
2025-02-23 21:42:46 -0500

    updated config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SoftballStatsApp/', // Set this to your GitHub repository name
+  base: "/SoftballStatsApp/", // Set this to your GitHub repository name
 });
